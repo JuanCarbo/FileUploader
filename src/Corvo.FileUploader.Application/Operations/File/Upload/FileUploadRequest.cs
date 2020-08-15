8f9111041acb21973430fee6f67eece887cbe226
Juan Carbó <juan.carbo@globallogic.com>
2020-08-15 00:21:48 -0300

    Added request class with contract verifications.

--- src/Corvo.FileUploader.Application/Operations/File/Upload/FileUploadRequest.cs
+++ src/Corvo.FileUploader.Application/Operations/File/Upload/FileUploadRequest.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Corvo.FileUploader.Application.Notifications;
+using Flunt.Notifications;
+using Flunt.Validations;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 
 namespace Corvo.FileUploader.Application.Operations.File.Upload
 {
-    class FileUploadRequest
+    public class FileUploadRequest : Notifiable, IRequest<EntityResult<FileUploadResult>>
     {
+        public FileUploadRequest(string fileName, IFormFile formFile)
+        {
+            FileName = fileName;
+            FormFile = formFile;
+            Verify();
+        }
+
+        private void Verify()
+        {
+            AddNotifications(new Contract()
+                .IsNotNull(FormFile, "FormFile", "FormFile Should not be null.")
+                .IsNotNullOrEmpty(FileName, "FileName", "FileName should not be null"));
+        }
+
+        public string FileName { get; }
+        public IFormFile FormFile { get; }
     }
 }
