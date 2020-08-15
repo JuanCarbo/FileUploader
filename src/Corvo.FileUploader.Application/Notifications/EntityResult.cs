1f55cab73c18cc76974532d6188a0d8a4885327d
Juan Carbó <juan.carbo@globallogic.com>
2020-08-14 23:49:22 -0300

    Added flunt and entityResult classes for notification handling

---
+++ src/Corvo.FileUploader.Application/Notifications/EntityResult.cs
@@ -0,0 +1,16 @@
+﻿using Flunt.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Corvo.FileUploader.Application.Notifications
+{
+    public class EntityResult<T> : Result where T : class
+    {
+        public EntityResult(T data, IReadOnlyCollection<Notification> notifications) : base(notifications)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+    }
+}
---
+++ src/Corvo.FileUploader.Application/Notifications/Result.cs
@@ -0,0 +1,15 @@
+﻿using Flunt.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Corvo.FileUploader.Application.Notifications
+{
+    public class Result : Notifiable
+    {
+        public Result(IReadOnlyCollection<Notification> notifications)
+        {
+            AddNotifications(notifications);
+        }
+    }
+}
